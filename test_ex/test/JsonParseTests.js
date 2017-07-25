4127b02d36004ac7ce99eff899b2a96fb2cc08e1
GLOBAL\I045774 <lucian.pop@sap.com>
2017-07-25 12:27:35 +0300

    test2

--- test_ex/test/JsonParseTests.js
+++
@@ -1,7 +0,0 @@
-describe("JSON", function() {
-   describe(".parse()", function() {
-       it("should detect malformed JSON strings", function(){
-           //Test Goes Here
-       });
-   });
-});
--- test_ex/test/passwordValidatorTests.js
+++ test_ex/test/passwordValidatorTests.js
@@ -1,10 +1,23 @@
+var chai = require("chai");
+var passwordValidator = require("../modules/passwordValidator");
+
 //module level
+
 describe("passwordValidator", function() {
     //function level
    describe(".validPassword(password)", function() {
        // function test1
        it("should have minimum 8 characters length", function(){
-           //Test Goes Here
+           //implement test with know wrong value
+           var wrongPass = "1234567";
+           var valid = passwordValidator.validPassword(wrongPass);
+           //chai check
+           chai.expect(valid).to.be.equal(false);
+
+           //test with good value
+           var goodPassword = "12345678";
+           var valid2 = passwordValidator.validPassword(goodPassword);
+           chai.expect(valid2).to.be.equal(true);
        });
    });
 });
