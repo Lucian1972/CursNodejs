d4e3266062e3384cabee55ef3bcca6a662fcd7cc
GLOBAL\I045774 <lucian.pop@sap.com>
2017-07-25 10:23:21 +0300

    process

--- process_ex/index.js
+++ process_ex/index.js
@@ -1,10 +1,22 @@
 process.on('exit', function(code){
     // this code will not be executed
-    setTimeout(function(){
-        console.log("this will not run");
+     setTimeout(function(){
+         console.log("this will not run");
 
-    }, 0);
+     }, 0);
     console.log("about code", code);
 
 });
+console.log("end");
+
+process.on('beforeExit', function(code){
+    // this code will not be executed
+     setTimeout(function(){
+         console.log("this will run");
+
+     }, 0);
+    console.log("about beforeexit code", code);
+
+});
+
 console.log("end");
